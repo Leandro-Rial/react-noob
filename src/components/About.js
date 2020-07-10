89736ca563b8411633df61ff26432b1f98e26491
Leandro Rial <leandro_rial@outlook.com>
2020-07-10 16:04:34 -0300

    Primera version

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, facilis quaerat natus dolorem neque dolore eius voluptas voluptatibus excepturi quia placeat consequatur ex inventore totam aperiam, dignissimos atque, perspiciatis repudiandae minus ipsam corrupti? Fuga doloremque rerum ea debitis voluptatibus iusto pariatur ab asperiores omnis. Repellat?</p>
+        </div>
+    )
+}
+
+export default About
